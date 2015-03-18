73d804a8e640f7209af95c2f3bd4c449a067a5c5
Chris Kleban <ckleban@gmail.com>
2015-03-17 21:32:33 -0700

    Create role_mover.js

---
+++ role_mover.js
@@ -0,0 +1,63 @@
+var role_mover = {
+    
+    //TODO
+    base: function(creep) {
+    
+    
+  if (creep.memory.job_active!='true'){ 
+   	
+    if(creep.energy < 1) {
+	    //creep.memory.target="0";
+        /*
+		var sources = creep.room.find(Game.SOURCES);
+		var source = creep.pos.findNearest(Game.SOURCES);
+	    var target = creep.memory.target;
+	    if (target!=null){
+	        creep.moveTo(target);
+	        creep.memory.target=source;
+	    } else {
+	        //creep.moveTo(source);
+	    }
+	    */
+	    //var source = Game.getObjectById(creep.memory.orig_target);
+	    //var target = creep.memory.orig_target;
+	    //var target = Game.getObjectByName(creep.memory.orig_target);
+	    var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
+		//console.log(target);
+		//creep.moveTo(sources[0]);
+		creep.moveTo(target);
+		creep.pickup(target);
+		
+    
+    	
+	}
+	else {
+		creep.moveTo(Game.spawns.Spawn1);
+		creep.transferEnergy(Game.spawns.Spawn1);
+	}
+  }
+	
+		
+    },
+ 
+ 
+ 
+ 	mine_normal: function(creep,job,spot) {
+        //creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y);
+        
+        if(creep.energy < 1) {
+            var miner = Game.getObjectById(job.jobmemory.miner[0]);
+	        var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
+		    creep.moveTo(miner);
+		    creep.pickup(target);
+		
+    
+    	
+   	    } else {
+		  creep.moveTo(Game.spawns.Spawn1);
+	 	  creep.transferEnergy(Game.spawns.Spawn1);
+	    }
+    }
+};
+
+module.exports = role_mover;
