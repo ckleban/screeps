ea1277187f0a792028cb1996d6eb292d5aab9a28
Chris Kleban <ckleban@gmail.com>
2015-03-17 21:32:49 -0700

    Create role_ranger.js

---
+++ role_ranger.js
@@ -0,0 +1,31 @@
+var role_ranger = {
+    
+    //TODO
+    base: function(creep) {
+        
+        // find target (but exclude spawn gaurds in march2015 survival game)
+        var target = creep.pos.findClosest(Game.HOSTILE_CREEPS, {
+			filter: function(object) {
+				return object.hitsMax < 4999;
+			}
+		});
+	
+	    //var source = creep.pos.findNearest(Game.HOSTILE_CREEPS)
+	    if(target) {
+		   	creep.rangedAttack(target);
+    	}
+		
+    },
+ 
+ 
+ 
+ 	wallofranged: function(creep,job,spot) {
+        creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y);
+        
+    }
+};
+
+module.exports = role_ranger;
+
+
+
