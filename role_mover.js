6e7f35be889a7094ef8933b2d7e6438c81816e17
ckleban <ckleban@gmail.com>
2015-03-20 21:56:23 -0700

    1

--- role_mover.js
+++ role_mover.js
@@ -26,20 +26,34 @@ var role_mover = {
  	mine_normal: function(creep,job,spot,spawn) {
         //creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y);
         
-        
+        var source = Game.getObjectById(job.jobmemory.source);
+        var miner = Game.getObjectById(job.jobmemory.miner[0]);
+        var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
+		    
+        // if miner isn't in danger
         if (creep.energy < creep.energyCapacity && job.jobmemory.danger=="false") {
-            var source = Game.getObjectById(job.jobmemory.source);
-            var miner = Game.getObjectById(job.jobmemory.miner[0]);
-	        var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
-		    creep.moveTo(miner);
+            
+            // check if miner is near the source
+            var targets = miner.pos.findInRange(Game.SOURCES, 1);
+            // if yes
+            if(targets.length > 0) {
+                //if source has energy, move to miner
+                if (targets[0].energy>1){
+                    creep.moveTo(miner);
+                //if source has no energy, move to nearest energy on ground
+                } else {
+                    creep.moveTo(target);   
+                }
+            }
+            
+
 		    creep.pickup(target);
 		
-    
+        // if miner is in danger, move to safe area
         } else if (creep.energy < creep.energyCapacity && job.jobmemory.danger=="true") {
-            var source = Game.getObjectById(job.jobmemory.source);
             creep.moveTo(source.pos.x+5,source.pos.y-8);
             
-		    
+        // if mover is full, bring back energy
    	    } else {
             //creep.moveTo(Game.spawns.Spawn1);
             //creep.transferEnergy(Game.spawns.Spawn1);
