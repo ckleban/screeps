58aa267b80453a517f232ed114f0ee1f161e7fd6
ckleban <ckleban@gmail.com>
2015-03-21 00:01:29 -0700

    f

--- role_healer.js
+++ role_healer.js
@@ -29,14 +29,6 @@ var role_healer = {
         
         var source = Game.getObjectById(job.jobmemory.source);
         var ranger = Game.getObjectById(job.jobmemory.ranger[0]);
-        var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
-		    
-        // if ranger is alive and nearer the enemy
-        //if (ranger && ranger.pos.y>creep.pos.y && creep.energy < creep.energyCapacity && job.jobmemory.danger=="false") {
-            
-            
-            
-
             
         var target = creep.pos.findClosest(Game.MY_CREEPS, {
             filter: function(object) {
@@ -45,21 +37,57 @@ var role_healer = {
 		});
 		
         if (job.jobmemory.health="healthy"){
-            if(target !== null) {
-                creep.moveTo(target);
+            if(target) {
+                creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y+1);
             } else {
                 creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y-1);
             }
         } else {           
             creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y-1);
         }
-            
+         
+        
+        
+        
+        
+        
+        
+        ////////////HEALING
+        
+        // Ranged heal weakest Crrep
+        
+        
+        //Logic to find creep in range that has lowest health
+        var targets = creep.pos.findInRange(Game.MY_CREEPS, 3, {
+			filter: function(object) {
+				return object.hits < object.hitsMax;
+			}
+		});
+        if(targets.length > 0) {
+            var best_target = targets[0];
+            for (var l in targets) {
+                var target = targets[l];
+                if ((best_target.hitsMax+best_target.hits)>(target.hitsMax+target.hits)) {
+                   best_target = target;
+                }
+            }
+            creep.rangedHeal(best_target);
+        }   
+        
+        
+        // Close Heal closet creep
+        var closetarget = creep.pos.findClosest(Game.MY_CREEPS, {
+            filter: function(object) {
+                return object.hits < object.hitsMax;
+			}
+		});
         if(target !== null) {
             creep.heal(target);
-            creep.rangedHeal(target);
-		} else {
-			//this.idleDefence(creep);
-		}
+		} 
+        
+        
+        
+        
 		
 		
     },
@@ -96,27 +124,11 @@ var role_healer = {
                 return object.hits < object.hitsMax;
 			}
 		});
-		
         if(target !== null) {
             creep.heal(target);
-            //creep.rangedHeal(target);
 		} 
         
         
-        //Logic to find nearest creep that needs healing
-        /*var target = creep.pos.findClosest(Game.MY_CREEPS, {
-            filter: function(object) {
-                return object.hits < object.hitsMax;
-			}
-		});
-		if(target !== null) {
-            creep.heal(target);
-            creep.rangedHeal(target);
-		} else {
-			//this.idleDefence(creep);
-		}
-        */
-		
     }
 };
 
