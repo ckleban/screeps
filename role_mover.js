1474f3f4dcbc3159f38840e0684a4cea86677c3a
ckleban <ckleban@gmail.com>
2015-03-20 22:03:09 -0700

    f

--- role_mover.js
+++ role_mover.js
@@ -44,6 +44,9 @@ var role_mover = {
                 } else {
                     creep.moveTo(target);   
                 }
+            } else {
+                creep.moveTo(source.pos.x+5,source.pos.y-8);
+               
             }
             
 
