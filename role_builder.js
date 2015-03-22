0a0ad6d48540abba50ad567ee662b41cace5de9e
ckleban <ckleban@gmail.com>
2015-03-22 00:14:13 -0700

    1

--- role_builder.js
+++ role_builder.js
@@ -4,12 +4,25 @@ var role_builder = {
     base: function(creep) {
         
         // find target (but exclude spawn gaurds in march2015 survival game)
+        
+        
+        
         var target = creep.pos.findClosest(Game.CONSTRUCTION_SITES);
         if(target) {
-            creep.moveTo(target);
-            creep.build(target);
+            if(creep.energy < 1) {
+                var spawn = creep.pos.findNearest(Game.MY_SPAWNS);
+                creep.moveTo(spawn);
+                creep.pickup(spawn);
+            } else {
+                creep.moveTo(target);
+                creep.build(target);
+            }
+        } else {
+            creep.moveTo(42,5);
         }
-		
+        
+        
+        
     },
  
  
