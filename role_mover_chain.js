b32d6182979c8b830b903c389abd0621c2d11c2e
ckleban <ckleban@gmail.com>
2015-03-21 21:27:08 -0700

    1

---
+++ role_mover_chain.js
@@ -0,0 +1,76 @@
+var role_mover_chain = {
+    
+    //TODO
+    base: function(creep) {
+    
+    
+        if (creep.memory.job_active!='true'){ 
+   	
+            if(creep.energy < 1) {
+                var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
+                creep.moveTo(target);
+                creep.pickup(target);
+		
+            }
+            else {
+                creep.moveTo(Game.spawns.Spawn1);
+                creep.transferEnergy(Game.spawns.Spawn1);
+            }
+        }
+	
+		
+    },
+ 
+ 
+ 
+ 	mine_normal: function(creep,job,spot,spawn) {
+        //creep.moveTo(job.jobmemory.x+spot,job.jobmemory.y);
+        
+        var source = Game.getObjectById(job.jobmemory.source);
+        var miner = Game.getObjectById(job.jobmemory.miner[0]);
+        var target = creep.pos.findNearest(Game.DROPPED_ENERGY);
+		    
+        // if miner isn't in danger
+        if (miner && creep.energy < creep.energyCapacity && job.jobmemory.danger=="false") {
+            
+            // check if miner is near the source
+            var targets = miner.pos.findInRange(Game.SOURCES, 1);
+            // if yes
+            if(targets.length > 0) {
+                //if source has energy, move to miner
+                if (targets[0].energy>1){
+                    creep.moveTo(miner);
+                //if source has no energy, move to nearest energy on ground
+                } else {
+                    creep.moveTo(target);   
+                }
+            } else {
+                creep.moveTo(source.pos.x+5,source.pos.y-8);
+               
+            }
+            
+
+		    creep.pickup(target);
+		
+        // if miner is in danger, move to safe area
+        } else if (creep.energy < creep.energyCapacity && job.jobmemory.danger=="true") {
+            creep.moveTo(source.pos.x+5,source.pos.y-8);
+        
+            
+        // if miner is dead
+        } else if (!miner && creep.energy < creep.energyCapacity) {
+            //creep.moveTo(source.pos.x+5,source.pos.y-8);
+            creep.moveTo(target);
+            creep.pickup(target);
+            
+        // if mover is full, bring back energy
+   	    } else {
+            //creep.moveTo(Game.spawns.Spawn1);
+            //creep.transferEnergy(Game.spawns.Spawn1);
+            creep.moveTo(spawn);
+            creep.transferEnergy(spawn);
+	    }
+    }
+};
+
+module.exports = role_mover_chain;
