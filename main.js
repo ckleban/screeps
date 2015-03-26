a012096d132dba5b4236476c6127802714b3b38c
ckleban <ckleban@gmail.com>
2015-03-25 20:27:26 -0700

    1

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ for (var ii in Game.rooms){
     // if Arena mode, look for dropped energy
     if (room.mode=Game.MODE_ARENA){
         //console.log("ARENA TIME!!");
-        console.log("Arena MODE!!");
+        console.log("Arena MODE!! - " + room);
         var targets = room.find(Game.DROPPED_ENERGY);
         if(targets.length) {
             //creep.moveTo(targets[0]);
@@ -289,7 +289,7 @@ for(var name in Game.creeps) {
         } else if (creep.name=='GladTough'){
             require('role_arena').tough(creep); 
         } else {
-            console.log(creep.name);
+            //console.log(creep.name);
             require('role_arena').base(creep);
         }
 	}	
