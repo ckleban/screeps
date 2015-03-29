dfc948e278628139f661e94e704d04688747b955
ckleban <ckleban@gmail.com>
2015-03-28 23:20:45 -0700

    1

--- role_arena.js
+++ role_arena.js
@@ -89,7 +89,7 @@ var role_arena = {
                                             room=Memory.map[exitroom];
                                             //Memory.map[previousroom].exit
                                             if (room){
-                                                console.log(creep.room.name + " exit:"+entry+"goes to room:"+room.name);
+                                                console.log(creep.room.name + " exit:"+entry+"goes to room:"+exitroom);
                                                 console.log(Game.time-room.lastseentime);
                                                 timediff=Game.time-room.lastseentime;
                                             }   
