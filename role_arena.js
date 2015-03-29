eedb9d0dd5e1dd2b885db02d76d9cf75ad38b9b9
ckleban <ckleban@gmail.com>
2015-03-28 21:52:05 -0700

    1

--- role_arena.js
+++ role_arena.js
@@ -11,7 +11,7 @@ var role_arena = {
                 
             } else {
                 console.log("NEW ROOM");
-                Game.rooms[creep.memory.room].memory.test="test";
+                Game.rooms[creep.memory.room.name].memory.test="test";
             }
         
    	        console.log(creep.memory.leaving.name + " - " + creep.memory.room.name);
